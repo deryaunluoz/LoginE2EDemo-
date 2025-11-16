91d74e66ac0928add88bbdda7decea2dcc6c91fe
deryaunluoz <deryaunluoz@icloud.com>
2025-11-17 02:58:01 +0400

    feat: update index.js with github url

---
+++ index.js
@@ -0,0 +1,11 @@
+function cypressTestProjesi() {
+  const checklist = {
+    //github repo adresini gir: Örn: https://github.com/Workintech/hello-world
+    project_url: 'https://github.com/deryaunluoz/LoginE2EDemo-',
+  };
+
+  return checklist;
+}
+
+/* Bu satırdan sonraki satırları silmeyin! */
+module.exports = cypressTestProjesi;
